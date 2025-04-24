69ac4ce18e87f757dd1b6b52a39a08f7ccc2743d
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2025-04-24 07:51:59 -0300

    FIX: singleton implementation was calling __init__ twice

--- device_manager/manager_singleton.py
+++ device_manager/manager_singleton.py
@@ -9,5 +9,4 @@ class DeviceManagerSingleton(DeviceManager):
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(DeviceManagerSingleton, cls).__new__(cls)
-            cls._instance.__init__()
         return cls._instance
