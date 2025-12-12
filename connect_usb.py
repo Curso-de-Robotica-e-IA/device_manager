818d50609af2178de17e17e26d51715c5ee56e23
Jairton Filho <jsff@cin.ufpe.br>
2025-12-12 13:49:47 -0300

    Add connect_usb.py - usage example

---
+++ connect_usb.py
@@ -0,0 +1,18 @@
+from device_manager import DeviceManager
+
+manager = DeviceManager()
+
+network_devices = manager.connect_devices()
+print(network_devices)
+print(manager.connected_devices)
+
+for device in manager.connected_devices:
+    info = manager.get_device_info(device)
+    print(info.get_properties())
+    actions = manager.get_device_actions(device)
+    actions.home_button()
+
+manager.execute_adb_command("input keyevent 3",manager.connected_devices, )
+
+manager.clear()
+
